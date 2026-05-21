a12c13bf4d28e36f5b1cbd35bb07a065fd97a6b2
Jyothi Swaroop Ganapavarapu <136258150+Swaroop0724@users.noreply.github.com>
2026-05-21 01:06:46 -0500

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -811,7 +811,7 @@ if uploaded_file:
 def load_llm():
 
     return ChatGroq(
-        groq_api_key=os.getenv("GROQ_API_KEY"),
+        groq_api_key=st.secrets["GROQ_API_KEY"],
         model_name="llama-3.3-70b-versatile"
     )
 
